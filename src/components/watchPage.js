401da537a451f4bc0691a01acd7a8dcc631be330
Kane <LisaxKane@gmail.com>
2022-12-06 15:02:13 -0600

    added like count and a like button

--- src/components/watchPage.js
+++ src/components/watchPage.js
@@ -3,12 +3,18 @@ import "../style/watchPage.scss"
 import SearchResult from "./searchResult"
 import youtube from "../youtube"
 import ConvertToM from "../functions/kFormatter";
+import {FaRegThumbsUp} from 'react-icons/fa'
+import {FaRegThumbsDown} from 'react-icons/fa'
+import {RiShareForwardLine} from 'react-icons/ri'
+import {BiListPlus,BiDotsHorizontalRounded} from 'react-icons/bi'
+
 var moment = require('moment'); // require
 export default function WatchPage({setHomePage,selectedVid, setIsSideBar}) {
     const timeAgo = moment(selectedVid.vid.snippet.publishedAt).fromNow();
     const [channelPic, setChannelPic] = useState()
     const [subCount, setSubCount] = useState()
     const [viewCount, setViewCount] = useState()
+    const [likeCount,setLikeCount] = useState()
     const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
     const [duration, setDuration] = useState({
         hours: 0,
@@ -55,6 +61,7 @@ export default function WatchPage({setHomePage,selectedVid, setIsSideBar}) {
               maxResults: 1,
             }
           })
+          setLikeCount(response.data.items[0].statistics.likeCount)
           setViewCount(response.data.items[0].statistics.viewCount)
           const response2 = await youtube.get("/videos", {
             params: {
@@ -94,6 +101,7 @@ console.log(selectedVid)
 
             </iframe>
             <div className="Title">{selectedVid.vid.snippet.title}</div>
+            <div className="channelContainerAndYoutubeOptions">
             <div className="channelInfo">
             <img src={channelPic} alt="" />
             <div className="channelNameSubCount">
@@ -101,6 +109,16 @@ console.log(selectedVid)
             <div>{ConvertToM(subCount)} subscribers</div>
             </div>
             <div className="subscribeBtn">Subscribe</div>
+            </div>
+            <div className="youtubeOptions">
+                <div className="thumbs">
+                <div className="thumbsUp"><FaRegThumbsUp size={23} style={{strokeWidth: "30", stroke: "rgb(187, 184, 184)"}}/>{ConvertToM(likeCount)}</div>
+                <div className="thumbsDown"><FaRegThumbsDown size={23} style={{strokeWidth: "30", stroke: "rgb(187, 184, 184)"}}/></div>
+                </div>
+                <div className="share"><RiShareForwardLine size={20}  />Share</div>
+                <div className="save"><BiListPlus size={24}/>Save</div>
+                <div className="threeDot"><BiDotsHorizontalRounded/></div>
+            </div>
             </div>
             <div className="descriptionBox">
                 <div className="viewDate">
